1065e6967a78f983a0ac057fe928f43845723f6b
kyj93790 <kyj93790@naver.com>
2023-01-19 17:05:11 +0900

    [FEAT] Server: add allUser

--- Server.hpp
+++ Server.hpp
@@ -3,9 +3,14 @@
 #ifndef SERVER_HPP
 # define SERVER_HPP
 
+# include <string>
+# include <map>
+# include "User.hpp"
+
 class Server {
     private:
         int _fd;
+		std::map<std::string, User *> allUser;
 
         Server(const Server& server);
         Server& operator=(const Server& server);
